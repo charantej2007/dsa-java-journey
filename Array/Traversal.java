c945a4a180391370dce02c823e9cdbe3aef054fa
charantej2007 <charanteju2007@gmail.com>
2026-05-16 20:19:29 +0530

    Array - 1st topic Traversal (Date: 16/05/2026)

---
+++ Array/Traversal.java
@@ -0,0 +1,10 @@
+package Array;
+public class Traversal{
+    public static void main(String[] args){
+        int [] arr={1,5,6,4,58,2,4};
+
+        for(int i=1 ; i < arr.length ; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
